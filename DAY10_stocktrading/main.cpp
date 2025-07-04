f06722691c988eba4cc5d5b435f93da9ea08040d
jw.song <chokosal@naver.com>
2025-07-04 10:45:22 +0900

    remove selectStockBrocker from MockStockBrokerDriver

--- DAY10_stocktrading/main.cpp
+++ DAY10_stocktrading/main.cpp
@@ -10,7 +10,6 @@ using namespace std;
 // Mock 클래스 정의
 class MockStockBrokerDriver : public StockBrokerDriverInterface {
 public:
-    MOCK_METHOD(string, selectStockBrocker, (), (const, override));
     MOCK_METHOD(bool, login, (const string& id, const string& pass), (override));
     MOCK_METHOD(bool, buy, (const string& code, int price, int quantity), (override));
     MOCK_METHOD(bool, sell, (const string& code, int price, int quantity), (override));
