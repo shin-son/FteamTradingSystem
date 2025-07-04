7492897a17ba698dd4024631da4ffba48d6ce39e
shin.son <thstls110534@naver.com>
2025-07-04 09:33:43 +0900

    [feature] RED:selectStockBrocker

--- DAY10_stocktrading/main.cpp
+++ DAY10_stocktrading/main.cpp
@@ -1,4 +1,24 @@
 #include "gmock/gmock.h"
+#include <string>
+
+using namespace testing;
+using namespace std;
+
+class AutoTradingSystem {
+public:
+    std::string selectStockBrocker();                     // 증권사 선택
+};
+
+TEST(stockBrocker, ReturnKiwoomOrNemo) {
+    AutoTradingSystem ats;
+    string broker = ats.selectStockBrocker();
+
+    // 선택 가능한 증권사 리스트
+    vector<string> validBrockers = { "키움", "네모" };
+
+    // 리턴된 값이 유효한 증권사인지 확인
+    EXPECT_THAT(validBrockers, Contains(broker));
+}
 
 int main() {
 	::testing::InitGoogleMock();
